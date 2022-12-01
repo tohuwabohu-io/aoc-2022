d9110abaa687326e4725828a1df02b4124465f97
Stefan Poindl <stefan@tohuwabohu.io>
2022-12-01 14:21:27 +0100

    code cleanup; add descriptions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,27 @@
 import aoc.Day01
 
+/**
+ * Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on
+ * Christmas. For that, their favorite snack is a special type of star fruit that only grows deep in the jungle. The
+ * Elves have brought you on their annual expedition to the grove where the fruit grows.
+ *
+ * To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by December 25th. Although
+ * the Elves assure you that the grove has plenty of fruit, you decide to grab any fruit you see along the way, just in
+ * case.
+ *
+ * Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second
+ * puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+ *
+ */
 fun main() {
     println("Hello, aoc!")
 
     println("Day 1:")
     print("Part One\t")
-    println(Day01.part01()[0])
+    println(Day01.part01())
 
     print("Part Two\t")
     println(Day01.part02())
+
+    // TODO: next day
 }
