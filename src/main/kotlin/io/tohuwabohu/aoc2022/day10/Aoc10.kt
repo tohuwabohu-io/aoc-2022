df046f9dc8e9031fa4b61ab18f0b5d198d5c9d9d
Stefan Poindl <stefan@tohuwabohu.io>
2022-12-10 20:20:29 +0100

    some day 10 polishing

--- src/main/kotlin/io/tohuwabohu/aoc2022/day10/Aoc10.kt
+++ src/main/kotlin/io/tohuwabohu/aoc2022/day10/Aoc10.kt
@@ -4,7 +4,7 @@ fun part01(): Int {
     var register = 1
     var signalStrength = 0
 
-    getCycles().forEachIndexed { i, cycleValue ->
+    parseInstructions().forEachIndexed { i, cycleValue ->
         val index = i + 1
 
         if (index == 20 || (index > 20 && ((index - 20) % 40) == 0)) {
@@ -23,8 +23,8 @@ fun part02() {
     val screenWidth = 40
     var index = 0
 
-    getCycles().forEach{ cycleValue ->
-        if (index in spritePosition - 1 .. spritePosition + 1) {
+    parseInstructions().forEach { cycleValue ->
+        if (index in spritePosition - 1..spritePosition + 1) {
             print("#")
         } else {
             print(".")
@@ -35,33 +35,24 @@ fun part02() {
         if ((index + 1) == screenWidth) {
             println()
             index = 0
-        } else index ++
+        } else index++
     }
 }
 
-private fun getCycles(): MutableList<Int> {
+private fun parseInstructions(): Sequence<Int> {
     val reader = io.tohuwabohu.aoc2022.util.getBufferedReader("src/main/resources/aoc10.txt")
 
-    val commands = reader.lineSequence()
+    return reader.lineSequence()
         .map { line -> line.split(" ") }
-        .map { parts -> if (parts.size > 1) Cycle(value = parts[1].toInt(), ttc = 2) else Cycle(value = 0, ttc = 1) }
-        .toList()
-
-    val cycles = mutableListOf<Int>()
-
-    commands.forEachIndexed { index, cycle ->
-        val previous = if (index > 0) commands[index - 1] else null
-
-        previous?.let { c ->
-            if (c.ttc == 1) {
-                cycles.add(cycle.value)
+        .mapIndexed { index, parts -> // first cycle has one extra step
+            if (parts.size > 1) {
+                Cycle(value = parts[1].toInt(), ttc = if (index == 0) 2 + 1 else 2)
             } else {
-                cycles.addAll(listOf(0, cycle.value))
+                Cycle(value = 0, ttc = if (index == 0) 1 + 1 else 1)
             }
-        } ?: cycles.addAll(listOf(0, cycle.value)) // <- that's the first one
-    }
-
-    return cycles
+        }
+        .zipWithNext()
+        .flatMap { pair -> if (pair.first.ttc == 2) listOf(pair.first.value, 0) else listOf(pair.first.value) }
 }
 
 private class Cycle(val value: Int, val ttc: Int)
