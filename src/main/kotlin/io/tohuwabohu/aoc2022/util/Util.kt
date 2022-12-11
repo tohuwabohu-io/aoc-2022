908bef20a9613222329e0a46ed1852358e256930
Stefan Poindl <stefan@tohuwabohu.io>
2022-12-11 12:58:50 +0100

    add multiplyInt convenience function similar to sumOf

--- src/main/kotlin/io/tohuwabohu/aoc2022/util/Util.kt
+++ src/main/kotlin/io/tohuwabohu/aoc2022/util/Util.kt
@@ -5,3 +5,12 @@ import java.io.File
 
 fun getBufferedReader(fileName: String): BufferedReader
         = File(fileName).bufferedReader()
+
+inline fun <T> Iterable<T>.multiplyInt(selector: (T) -> Int): Int {
+    this.firstOrNull() ?: return 0
+    var result = 1
+    for (element in this) {
+        result *= selector(element)
+    }
+    return result
+}
